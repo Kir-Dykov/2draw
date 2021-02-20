9b6d32d3ae611853d31d83118e8816125265c1d7
Mirali77 <mirali.agaev04@yandex.ru>
2021-02-21 00:44:23 +0300

    TochkaFixed

--- 2draw/2draw/point.h
+++ 2draw/2draw/point.h
@@ -4,13 +4,13 @@ using namespace std;
 class point
 {
 public:
-	void set_point(int, int);   //задание точки:													p.set_point()
-	int find_quarter();    //нахождение четверти, в которой находится точка							p.find_quarter() 
-	void cout_point();     //вывести координаты точки в консоль										p.cout_point()
-double x, y;	//координаты x, y
+	void set_point(int, int);   //zadanie tochki:													p.set_point()
+	int find_quarter();    //nahozhdenie chetverti v kotoroy nahoditsa tochka:						p.find_quarter() 
+	void cout_point();     //vivod koordinat tochki d konsol:										p.cout_point()
+double x, y;	//koordinati x y
 };
 
-//Функции от точек:
-point sym_point_x(point);   //создание точки, симметричной заданной относительно оси Оx				q = sym_point_x(p)
-point sym_point_y(point);   //создание точки, симметричной заданной относительно оси Оy				q = sym_point_y(p)
-double point_distance(point p1, point p2); //вычисление расстояния между заданными точками			r = point_distance(p1, p2)
+//Funkcii ot tochek:
+point sym_point_x(point);   //sozdanie tochki, simmetrichnoi zadannoi otnositelno osi Ox:			q = sym_point_x(p)
+point sym_point_y(point);   //sozdanie tochki, simmetrichnoi zadannoi otnositelno osi Оy:			q = sym_point_y(p)
+double point_distance(point p1, point p2); //vichislenie rasstoyaniya mezhdy tochkami:				r = point_distance(p1, p2)
