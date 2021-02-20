56bd96a75ac5bb4d5a44ef65fea8a48f810b27fc
Mirali77 <mirali.agaev04@yandex.ru>
2021-02-21 00:37:13 +0300

    Ppppppoint

--- 2draw/2draw/point.h
+++ 2draw/2draw/point.h
@@ -1,1 +1,16 @@
 #pragma once
+#include <iostream>
+using namespace std;
+class point
+{
+public:
+	void set_point(int, int);   //задание точки:													p.set_point()
+	int find_quarter();    //нахождение четверти, в которой находитс€ точка							p.find_quarter() 
+	void cout_point();     //вывести координаты точки в консоль										p.cout_point()
+double x, y;	//координаты x, y
+};
+
+//‘ункции от точек:
+point sym_point_x(point);   //создание точки, симметричной заданной относительно оси ќx				q = sym_point_x(p)
+point sym_point_y(point);   //создание точки, симметричной заданной относительно оси ќy				q = sym_point_y(p)
+double point_distance(point p1, point p2); //вычисление рассто€ни€ между заданными точками			r = point_distance(p1, p2)
