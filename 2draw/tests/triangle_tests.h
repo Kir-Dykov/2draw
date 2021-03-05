4288bb4fd1c41e4e743ba59e692b5e1057078a98
SibPr <dengo123455@gmail.com>
2021-03-05 16:06:42 +0300

    added tests for triangle

---
+++ 2draw/tests/triangle_tests.h
@@ -0,0 +1,31 @@
+#pragma once
+#include "../triangle.h"
+
+using namespace std;
+
+int triangle_main() 
+{
+	triangle t;
+	Line bis;
+	Circle cir;
+
+	Point p1, p2, p3;
+	p1.set_point(0, 0);
+	p2.set_point(2, 2);
+	p3.set_point(3, 2);
+	t.set_triangle(p1, p2, p3);
+
+	cout << "Triangle area?" << t.triangle_area() << endl;
+
+	cout << "Triangle bisectrix: ";
+	bis = t.get_bisectrix(2);
+	bis.cout_line();
+	cout << endl;
+
+	cout << "Tetragon circumcircle: ";
+	cir = t.get_circumcircle();
+	cir.cout_circle(cir);
+	cout << endl;
+
+	return 0;
+}
