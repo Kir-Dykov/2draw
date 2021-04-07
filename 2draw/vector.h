da17e0dd3d498fee70031a6e9b52501a33ac3a9f
Kir-Dykov <danila.bain@yandex.com>
2021-04-07 11:59:53 +0300

    const's in vector

--- 2draw/vector.h
+++ 2draw/vector.h
@@ -12,16 +12,16 @@ public:
 
 	void set(double _x, double _y) { x = _x; y = _y; }
 
-	bool operator==(Vector other) { return other.x==x && other.y==y; }
-	bool operator!=(Vector other) { return other.x != x || other.y != y; }
+	bool operator==(const Vector other) const{ return other.x==x && other.y==y; }
+	bool operator!=(const Vector other) const{ return other.x != x || other.y != y; }
 
-	Vector operator+(Vector other) { return Vector(x + other.x, y + other.y); }
-	Vector operator-(Vector other) { return Vector(x - other.x, y - other.y); }
-	double operator*(Vector other) { return x * other.x + y * other.y; } //dot product
-	Vector operator*(double lambda) { return Vector(x*lambda, y*lambda); } // miltiplication by a number
+	Vector operator+(const Vector other) const{ return Vector(x + other.x, y + other.y); }
+	Vector operator-(const Vector other) const{ return Vector(x - other.x, y - other.y); }
+	double operator*(const Vector other) const{ return x * other.x + y * other.y; } //dot product
+	Vector operator*(const double lambda)const{ return Vector(x*lambda, y*lambda); } // miltiplication by a number
 
-	double abs() { return sqrt(x * x + y * y); }
-	double arg() { return atan2(x, y); }
+	double abs() const{ return sqrt(x * x + y * y); }
+	double arg() const{ return atan2(x, y); }
 
 	friend double determinant(Vector, Vector); // determinant of the 2x2 matrix formed by two vector-column's
 	friend double dot(Vector, Vector);
