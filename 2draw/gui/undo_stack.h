0e27401c1bb6e2889e9a0b5fb55b8e1e2f561de0
Kir-Dykov <danila.bain@yandex.com>
2021-06-04 21:46:39 +0300

    undo_stack.h refactoring, one more memory leak fix

--- 2draw/gui/undo_stack.h
+++ 2draw/gui/undo_stack.h
@@ -19,85 +19,86 @@ private:
 			this->pPrev = pPrev;
 		}
 	};
+
+	//delete every element above (including) given Node
 	void truncate(Node*);
-public:
+
+	// the last element we store
 	Node* head;
+
+	// the element we "looking" at
 	Node* top;
 
+public:
+	
 	UndoStack();
 	~UndoStack();
 
+	// add element after top
 	void push_back(T);
 
+	// read the top value, move top one step down
 	T undo();
+
+	// move top one step up, read the top value
 	T redo();
-	//удаление с текущего элемента и до конца
+	
 	
 };
 
 template<typename T>
-UndoStack<T>::UndoStack()
-{
+UndoStack<T>::UndoStack() {
 	head = nullptr;
 	top = nullptr;
-	push_back(T());
 }
 
 template<typename T>
-UndoStack<T>::~UndoStack()
-{
+UndoStack<T>::~UndoStack() {
 	truncate(head);
 }
 
 template<typename T>
-void UndoStack<T>::truncate(Node* elem)
-{
+void UndoStack<T>::truncate(Node* elem) {
 	Node* cur = elem;
 	if (cur != nullptr)
 	{
-		if (cur->pPrev == nullptr)
-		{
-			delete cur;
-			head = nullptr;
-			top = nullptr;
-		}
-		else
-		{
+		if (cur->pPrev != nullptr) {
 			cur->pPrev->pNext = nullptr;
-			Node* tmp = cur;
-			while (cur->pNext != nullptr)
-			{
-				cur = cur->pNext;
-				delete tmp;
-				tmp = cur;
-			}
-			delete cur;
 		}
+
+		//delete everything to the end
+		Node* tmp = cur;
+		while (cur->pNext != nullptr)
+		{
+			cur = cur->pNext;
+			delete tmp;
+			tmp = cur;
+		}
+		delete cur;
+		
 	}
 }
 
 template<typename T>
-void UndoStack<T>::push_back(T elem)
-{
+void UndoStack<T>::push_back(T elem) {
+
 	Node* tmp = new Node(elem);
+
 	if (top != nullptr)
 	{
 		if (top->pNext != nullptr)
 			truncate(top->pNext);
 		tmp->pPrev = top;
 		top->pNext = tmp;
-		top = tmp;
 	}
-	else
-	{
-		head = tmp;
-		top = tmp;
-	}
+
+	head = tmp;
+	top = tmp;
+
 }
 
 template<typename T>
-T UndoStack<T>::undo()
-{
+T UndoStack<T>::undo() {
 	T tmp = top->data;
 	if (top->pPrev != nullptr)
 		top = top->pPrev;
@@ -105,11 +106,8 @@ T UndoStack<T>::undo()
 }
 
 template<typename T>
-T UndoStack<T>::redo()
-{
+T UndoStack<T>::redo() {
 	if (top->pNext != nullptr)
-	{
 		top = top->pNext;
-	}
 	return top->data;
 }
